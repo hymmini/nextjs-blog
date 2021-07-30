7742514c8891917a076a0610df421aea4c8f8a4f
IVYBRIDGE\mindyh <mindy.hu@ivybridge.cn>
2021-07-30 17:20:31 +0800

    use custome app,js config default seo

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,16 @@
 import "../styles/global.css";
-import { AppProps } from "next/app";
-import { appWithTranslation } from 'next-i18next'
+import { AppProps,Container  } from "next/app";
+import { appWithTranslation } from 'next-i18next';
+import { DefaultSeo } from 'next-seo';
+import SEO from '../next-seo.config';
 
 function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return(
+    <Container>
+      <DefaultSeo {...SEO} />
+      <Component {...pageProps} />
+    </Container>
+  ) ;
 }
 
 export default appWithTranslation(App);
