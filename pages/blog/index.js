b645a477351461c65bab622face5336bdfe12534
IVYBRIDGE\mindyh <mindy.hu@ivybridge.cn>
2021-07-30 17:11:52 +0800

    install next-seo, config blog page with different title and description

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -1,5 +1,14 @@
 import Layout from "../../components/layout";
+import { NextSeo } from 'next-seo';
 
 export default function blog() {
-  return <Layout>This is a Blog page</Layout>;
+  return <Layout>
+    <>
+      <NextSeo
+        title="Simple Usage Example"
+        description="A short description goes here."
+      />
+    </>
+    This is a Blog page
+  </Layout>;
 }
